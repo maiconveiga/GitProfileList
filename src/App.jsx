0e47932d2b82f05e85ac715971ad99d400e39946
Maicon Veiga <35174561+maiconveiga@users.noreply.github.com>
2024-03-20 11:52:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="container">
 
-      <input placeholder="Insira seu usuario git ex:maiconveiga" type="text" onBlur={(e) => { setNomeUsuaro(e.target.value) }} />
+      <input placeholder="Insira gituser ex:maiconveiga" type="text" onBlur={(e) => { setNomeUsuaro(e.target.value) }} />
       {nomeUsuario.length > 4 && (
         <>
           <Perfil nome={nomeUsuario} />
